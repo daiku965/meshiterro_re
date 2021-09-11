7c94ccfc80bfbf6994ce3f90fb32790d9742c86b
daiku965 <daiku965@gmail.com>
2021-09-11 08:56:03 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd150fdccce1907befd9054de8d0aa05dead69369a1948ccf0f01aa0ab3b8b209e65469c73dbd071b456dcd19e1090e592ab4c988da7916b41baded6c73e669c5'
